6a5495c633744a5871df09bb4968f2170bf11fe4
aoikurokawa <ht.so.ik0102@gmail.com>
2026-03-30 05:11:12 +0900

    feat: config

--- program/src/error.rs
+++ program/src/error.rs
@@ -1,1 +1,14 @@
+use pinocchio::error::ProgramError;
+use thiserror::Error;
 
+#[derive(Debug, Error)]
+pub enum SenshiError {
+    #[error("Encountered an arithmetic under/overflow error.")]
+    ArithmeticError,
+}
+
+impl From<SenshiError> for ProgramError {
+    fn from(value: SenshiError) -> Self {
+        Self::Custom(value as u32)
+    }
+}
